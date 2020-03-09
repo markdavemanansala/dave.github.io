f7eda9aff0e796fde6ec82293fd4b96dab375647
The Blockchain Pundit <davegala09@gmail.com>
2020-03-09 13:58:10 +0800

    Updates

--- asd.php
+++ asd.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
      
     // CHANGE THE TWO LINES BELOW
-    $email_to = "appsofdave@gmail.com";
+    $email_to = "davegala09@gmail.com";
      
     $email_subject = "website html form submissions";
      
